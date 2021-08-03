441d76f8355acef2d3d6c82d15d3dc449e2217f2
Daniele Scipioni <daniele.scipioni@gmail.com>
2021-08-03 17:30:44 +0200

    workaround

---
+++ MemoryLeak/StringListView.xaml.cs
@@ -0,0 +1,25 @@
+﻿using MemoryLeak.Views;
+using Windows.UI.Xaml.Controls;
+
+namespace MemoryLeak
+{
+    public sealed partial class StringListView
+    {
+        private StringList _stringList;
+
+        public StringListView()
+        {
+            InitializeComponent();
+            DataContextChanged += (sender, args) =>
+            {
+                if (_stringList == args.NewValue) return;
+                _stringList = (StringList) args.NewValue;
+                Bindings.Update();
+            };
+        }
+
+        private void ListView_OnItemClick(object sender, ItemClickEventArgs e)
+        {
+        }
+    }
+}
